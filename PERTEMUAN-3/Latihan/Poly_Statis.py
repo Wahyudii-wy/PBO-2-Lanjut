ed57047ce13b16ba10c8c9d0ea97fb66a8c681b6
Wahyudi <102577510+Cuyud@users.noreply.github.com>
2023-03-30 06:02:31 -0700

    Add files via upload

---
+++ PERTEMUAN-3/Latihan/Poly_Statis.py
@@ -0,0 +1,22 @@
+#Contoh 1: Penggunaan fungsi “len” pada berbagai tipe data:
+print(len("Hello")) 
+print(len([1, 2, 3])) 
+print(len((1, 2, 3)))
+
+#Contoh 2: Penggunaan operator “+” pada berbagai tipe data:
+print(2 + 3) 
+print("Hello" + " World") 
+print([1, 2] + [3, 4]) 
+
+#Contoh 3: Penggunaan fungsi “max” pada berbagai tipe data:
+print(max(2, 5))
+print(max([1, 2, 3])) 
+print(max("Hello")) 
+
+#Contoh 4: Penggunaan metode “sort” pada berbagai tipe data:
+a = [3, 1, 2]
+a.sort()
+print(a)
+b = ["c", "a", "b"]
+b.sort()
+print(b) 
