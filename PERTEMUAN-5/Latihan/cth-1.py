8bffb13153d4e0803e000e0ec156d457481a8486
Wahyudi <102577510+Cuyud@users.noreply.github.com>
2023-04-16 07:26:05 -0700

    Add files via upload

---
+++ PERTEMUAN-5/Latihan/cth-1.py
@@ -0,0 +1,8 @@
+# Contoh 1: Mengatasi Type Error
+try:
+    x = "Hello"
+    y = x + 5
+except TypeError:
+    print("Terjadi kesalahan tipe data, pastikan variabel yang digunakan sudah benar!")
+
+
